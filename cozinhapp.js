6d6758fd7940fa559490f74b852b68be3ceec3b1
Sérgio Lopes <sergio.lopes@caelum.com.br>
2015-12-29 11:49:50 -0200

    refinando as collections locais

--- cozinhapp.js
+++ cozinhapp.js
@@ -1,32 +1,31 @@
 Pedidos = new Mongo.Collection("pedidos");
 
-if (Meteor.isClient) {
-  
-  var lastHour = new Date();
-  lastHour.setHours(lastHour.getHours()-1);
-
-  Template.body.helpers({
-    pedidos: function () {
-      return Pedidos.find({"ativo": true, "createdAt":{$gt: lastHour},}, {sort: {createdAt: -1}});
-    }
-  });
-
-  Template.pedido.events({
-    "click .delete": function () {
-      Pedidos.update(this._id, {
-        $set: {ativo: false}
-      });
-    }
-  });
-
-  Template.registerHelper('horario', function(date) {
-    return (date.getHours() + 100 + '').substr(1) + ':' + (date.getMinutes() + 100 + '').substr(1);
-  });
-
-}
-
+Meteor.methods({
+  finalizar: function (id) {
+    Pedidos.update(id, {
+      $set: {ativo: false}
+    });
+  }
+});
 
 if (Meteor.isServer) {
+  
+  Meteor.publish("pedidos", function () {
+    var lastHour = new Date();
+    lastHour.setHours(lastHour.getHours()-1);
+
+    return Pedidos.find(
+      {
+        "ativo": true, 
+        "createdAt": {$gt: lastHour}
+      }, 
+      {
+        sort: {createdAt: -1}
+      }
+    );
+
+  });
+
   Picker.route('/novo-pedido', function(params, req, res, next) {
     
     res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
@@ -54,4 +53,58 @@ if (Meteor.isServer) {
     }
    
   });
+
+  Picker.route('/produtos', function(params, req, res, next) {
+    
+    res.setHeader('Content-Type', 'application/json; charset=UTF-8');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    var resposta = {
+      bolos: [
+        {
+          subcategoria: "Fatia",
+          produtos: ["Só de Cenoura", "Com Nutella", "De Brigadeiro", "Açucarado"]
+        },{
+          subcategoria: "Pra levar",
+          produtos: ["Só de Cenoura inteiro", "Com Nutella inteiro", "De Brigadeiro inteiro", "Açucarado inteiro"]
+        }
+      ],
+      bebidas: [
+        {
+          subcategoria: "Cafés",
+          produtos: ["Espresso", "Capuccino", "Mocachino"]
+        },{
+          subcategoria: "Refrigerantes",
+          produtos: ["Soda", "Guaraná", "Coca"]
+        },{
+          subcategoria: "Sucos",
+          produtos: ["Melancia", "Tangerina", "Limão"]
+        }
+      ]
+    };
+    
+    res.end(JSON.stringify(resposta));
+  });
+}
+
+
+if (Meteor.isClient) {
+  Meteor.subscribe("pedidos");
+  
+  Template.body.helpers({
+    pedidos: function () {
+      return Pedidos.find();
+    }
+  });
+
+  Template.pedido.events({
+    "click .delete": function () {
+      Meteor.call("finalizar", this._id);
+    }
+  });
+
+  Template.registerHelper('horario', function(date) {
+    return (date.getHours() + 100 + '').substr(1) + ':' + (date.getMinutes() + 100 + '').substr(1);
+  });
+
 }
