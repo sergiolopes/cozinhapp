9f6618df98e0d700dca94ea4658aaab391f0d2a0
Sérgio Lopes <sergio.lopes@caelum.com.br>
2016-01-18 16:13:03 -0200

    random

--- cozinhapp.js
+++ cozinhapp.js
@@ -81,6 +81,11 @@ if (Meteor.isServer) {
         }
       ];
 
+    if (!!params.query.random) {
+      resposta = resposta.concat(resposta);
+      shuffle(resposta);
+    }
+
     resposta.forEach(function(bolo, index){
       bolo.id = index;
     });
@@ -111,4 +116,24 @@ if (Meteor.isClient) {
     return (date.getHours() + 100 + '').substr(1) + ':' + (date.getMinutes() + 100 + '').substr(1);
   });
 
+}
+
+function shuffle(array) {
+    var counter = array.length, temp, index;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
 }
